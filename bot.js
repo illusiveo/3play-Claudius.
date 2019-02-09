f9649ccd6de1b22d5429b4e6a5eab4648d085f25
illusiveo <46373282+illusiveo@users.noreply.github.com>
2019-02-09 15:30:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,6 +48,8 @@ LET'S GO!
     }, 100000);
 });
 
+// awooooo command.
+
 const { Command } = require('klasa');
 
 module.exports = class AwooCommand extends Command {
@@ -62,6 +64,30 @@ module.exports = class AwooCommand extends Command {
 	async run(msg) {
 		const { url } = await this.wolkeClient.getRandom({ type: this.name, hidden: false, nsfw: false, filetype: 'gif' });
 		return msg.send(new this.client.methods.Embed().setImage(url));
+	}
+};
+
+// hug command.
+
+const { Command } = require('klasa');
+
+
+module.exports = class HugCommand extends Command {
+	constructor(...args) {
+		super(...args, {
+			cooldown: 5,
+			usage: '[member:member]',
+			botPerms: ['EMBED_MESSAGE'],
+			description: 'Hug someone or get hugged'
+		});
+	}
+
+	async run(msg, [member]) {
+		const { url } = await this.wolkeClient.getRandom({ type: this.name, hidden: false, nsfw: false, filetype: 'gif' });
+		return msg.send(new this.client.methods.Embed()
+			.setDescription(member ? `${msg.member} hugged ${member}` : `${msg.member} got hugged`)
+			.setImage(url)
+		);
 	}
 };
 
